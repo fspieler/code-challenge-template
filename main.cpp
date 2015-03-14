aab0b0aae5d01f2bdf41c3e45d8a3ff1e6fd2ecd
Fred Spieler <fred@fredspieler.com>
2015-03-14 12:56:14 -0400

    Added a default loop for handling test cases

--- main.cpp
+++ main.cpp
@@ -9,5 +9,12 @@ using namespace std;
 
 int main(){
 
+    int num_test_cases;
+    cin >> num_test_cases;
+    for(int test_case = 1; test_case <= num_test_cases; ++test_case){
+
+        // FEED ME CODE
+
+    }
     return 0;
 }
