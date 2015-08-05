f2eed94484e2bd4f7ad6f077ff4bea3432023031
Fred Spieler <fspieler@gmail.com>
2015-08-05 00:05:30 -0400

    Add branch for Bloomberg
    Similar to gcj branch, but different expected format for input and
    output; there really is more that could be shared between these branches
    but I'll deal with that later.

---
+++ bloomberg-code-contest/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <cstdio>
+#include <vector> 
+#include <algorithm> 
+
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+
+    //FEED ME CODE
+
+    return 0;
+}
