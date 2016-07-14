97a12d656f11d95c936b2fe81cfba91366998ee7
Frederic Spieler <fspieler@gmail.com>
2016-07-14 14:32:52 -0400

    Fix includes and remove clargs

--- bloomberg-code-contest/main.cpp
+++ bloomberg-code-contest/main.cpp
@@ -1,16 +1,15 @@
-#include <challenge_includes.h>
 #include <iostream>
 #include <string>
 #include <cstdio>
-#include <vector> 
-#include <algorithm> 
+#include <vector>
+#include <algorithm>
+
+#include <includes.h> // contest libs
 
 using namespace std;
 
-int main(int argc, char* argv[])
+int main()
 {
-
     /* FEED ME CODE */
-
     return 0;
 }
