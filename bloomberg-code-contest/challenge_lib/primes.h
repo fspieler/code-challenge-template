5e9eda03b5518f34a0dbc4ac7e481f0b0698b495
Frederic Spieler <fspieler@gmail.com>
2016-07-17 22:48:46 -0400

    Reorganize primes and move gcd, lcm here

--- bloomberg-code-contest/challenge_lib/primes.h
+++ bloomberg-code-contest/challenge_lib/primes.h
@@ -1,67 +1,129 @@
 #ifndef PRIMES_INCLUDES_GUARD
 #define PRIMES_INCLUDES_GUARD
 
+#include <types.h>
 #include <debug.h>
 #include <vector>
+#include <tuple>
 #include <algorithm>
 
-bool isPrime(unsigned long long n)
+class Prime
 {
-  if(n==2)return true;
-  if(n%2 == 0 || n ==1){return false;}
-  for(unsigned long long i = 3; i <= (unsigned long long)sqrt(n+1); i+=2)
+public:
+  virtual bool isPrime(ull n)
   {
-    if(n % i == 0){return false;}
+    if(n==2)return true;
+    if(n%2 == 0 || n ==1){return false;}
+    for(ull i = 3; i <= (ull)sqrt(n+1); i+=2)
+    {
+      if(n % i == 0){return false;}
+    }
+    return true;
   }
-  return true;
-}
-
-namespace _isPrimeMemoized {
-  unsigned long long highest_prime = 3;
-  std::vector<unsigned long long> primes = {2,3};
 };
 
-bool isPrimeMemoized(unsigned long long n)
+bool isPrime(ull n)
 {
-  if(n <= 1){return false;}
-
-  auto &highest_prime = _isPrimeMemoized::highest_prime;
-  auto &primes = _isPrimeMemoized::primes;
-
-  if(n < highest_prime)
-  {
-      return binary_search(primes.begin(),primes.end(),n);
-  }
-  auto square_root = (unsigned long long) sqrt(n+1);
-  
-  for
-  (
-   auto prime = begin(primes);
-   prime != end(primes) && *prime <= square_root;
-   ++prime
-  )
-  {
-    if(n % *prime == 0)
-    {
-      return false;
-    }
-  }
-
-  for(unsigned long long i = highest_prime+2;
-          i <= square_root;
-          i+=2)
-  {
-    if(isPrimeMemoized(i))
-    {
-      primes.push_back(i);
-      highest_prime = i;
-    }
-    if(n % i == 0)
-    {
-      return false;
-    }
-  }
-  return true;
+  Prime p;
+  return p.isPrime(n);
 }
 
+class MemoizedPrime : public Prime
+{
+  ull highest_prime = 3;
+  std::vector<ull> primes = {2,3};
+public:
+  bool isPrime(ull n)
+  {
+    if(n <= 1){return false;}
+
+    if(n < highest_prime)
+    {
+      return binary_search(primes.begin(),primes.end(),n);
+    }
+    auto square_root = (ull) sqrt(n+1);
+
+    for
+      (
+       auto prime = begin(primes);
+       prime != end(primes) && *prime <= square_root;
+       ++prime
+      )
+      {
+        if(n % *prime == 0)
+        {
+          return false;
+        }
+      }
+
+    for(ull i = highest_prime+2;
+        i <= square_root;
+        i+=2)
+    {
+      if(isPrime(i))
+      {
+        primes.push_back(i);
+        highest_prime = i;
+      }
+      if(n % i == 0)
+      {
+        return false;
+      }
+    }
+    return true;
+  }
+  std::vector<std::pair<ull, int> > getPrimeFactors(ull n)
+  {
+    std::vector<std::pair<ull, int> > ret;
+    if(isPrime(n))
+    {
+      ret.push_back(std::make_pair(n,1));
+      return ret;
+    }
+    for(auto i = 0; n > 1; ++i)
+    {
+      size_t count = 0;
+      while(0 == n % primes[i])
+      {
+        n /= primes[i];
+        count++;
+      }
+      if(count)
+      {
+        ret.push_back(std::make_pair(primes[i],count));
+      }
+    }
+    return ret;
+  }
+
+};
+
+namespace _MemoizedPrime
+{
+  MemoizedPrime mp;
+}
+
+bool isPrimeMemoized(ull n)
+{
+  return _MemoizedPrime::mp.isPrime(n);
+}
+/**
+ * Returns a vector of prime factors
+ */
+std::vector<std::pair<ull, int> > getPrimeFactors(ull n)
+{
+  return _MemoizedPrime::mp.getPrimeFactors(n);
+}
+
+ull gcd(ull a, ull b)
+{
+  // this impl is not my own... found online a while back, can't find source
+  while(b) b ^= a ^= b ^= a %= b;
+  return a;
+}
+
+ull lcm(ull first, ull second)
+{
+  return first*second/gcd(first,second);
+}
 #endif //PRIMES_INCLUDES_GUARD
--- bloomberg-code-contest/challenge_lib/primes.t.cpp
+++ bloomberg-code-contest/challenge_lib/primes.t.cpp
@@ -15,18 +15,53 @@ TEST(primes,agree_upto_1million){
 }
 
 TEST(primes,arbitrary_small_primes){
-  EXPECT_EQ(isPrimeMemoized(25),false);
-  EXPECT_EQ(isPrimeMemoized(27),true);
-  EXPECT_EQ(isPrimeMemoized(9),false);
-  EXPECT_EQ(isPrimeMemoized(1),false);
-  EXPECT_EQ(isPrimeMemoized(2),true);
-  EXPECT_EQ(isPrimeMemoized(3),true);
-  EXPECT_EQ(isPrimeMemoized(4),false);
-  EXPECT_EQ(isPrimeMemoized(13),true);
+  EXPECT_EQ(false, isPrimeMemoized(25));
+  EXPECT_EQ(true, isPrimeMemoized(29));
+  EXPECT_EQ(false, isPrimeMemoized(9));
+  EXPECT_EQ(false, isPrimeMemoized(1));
+  EXPECT_EQ(true, isPrimeMemoized(2));
+  EXPECT_EQ(true, isPrimeMemoized(3));
+  EXPECT_EQ(false, isPrimeMemoized(4));
+  EXPECT_EQ(true, isPrimeMemoized(13));
 }
-#if 0
+
 TEST(primes,arbitrary_large_primes){
-  test_isPrime(982'451'653,true);
-  test_isPrime(86'028'121,true);
+  EXPECT_EQ(true, isPrimeMemoized(982'451'653));
+  EXPECT_EQ(true, isPrimeMemoized(86'028'121));
 }
-#endif
+
+TEST(primefactors,prime_number_is_factored_properly_with_correct_order){
+  auto facs = getPrimeFactors(101);
+  EXPECT_EQ(1,facs.size());
+  EXPECT_EQ(101,std::get<0>(facs[0]));
+  EXPECT_EQ(1,std::get<1>(facs[0]));
+}
+
+TEST(primefactors,360_is_factored_properly_with_correct_order){
+  auto facs = getPrimeFactors(360);
+  EXPECT_EQ(3,facs.size());
+  EXPECT_EQ(2,std::get<0>(facs[0]));
+  EXPECT_EQ(3,std::get<1>(facs[0]));
+  EXPECT_EQ(3,std::get<0>(facs[1]));
+  EXPECT_EQ(2,std::get<1>(facs[1]));
+  EXPECT_EQ(5,std::get<0>(facs[2]));
+  EXPECT_EQ(1,std::get<1>(facs[2]));
+}
+
+TEST(primefactors,1000_is_factored_properly_with_correct_order){
+  auto facs = getPrimeFactors(1000);
+  EXPECT_EQ(2,facs.size());
+  EXPECT_EQ(2,std::get<0>(facs[0]));
+  EXPECT_EQ(3,std::get<1>(facs[0]));
+  EXPECT_EQ(5,std::get<0>(facs[1]));
+  EXPECT_EQ(3,std::get<1>(facs[1]));
+}
+
+TEST(gcd,return_gcd_with_two_inputs){
+  EXPECT_EQ(12,gcd(72,84));
+}
+
+TEST(lcm,return_lcm_with_two_inputs){
+  EXPECT_EQ(75,lcm(15,25));
+  EXPECT_EQ(4061,lcm(131,31));
+}
